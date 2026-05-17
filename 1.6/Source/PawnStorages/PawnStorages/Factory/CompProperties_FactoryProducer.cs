dfcf852d25e9244f0a46ff7c72a048a7e85e9a6d
Momonraga <dpmartin11@gmail.com>
2026-05-17 10:39:16 +0800

    Delete 1.6/Source/PawnStorages/PawnStorages/Factory directory

--- 1.6/Source/PawnStorages/PawnStorages/Factory/CompProperties_FactoryProducer.cs
+++
@@ -1,8 +0,0 @@
-﻿using Verse;
-
-namespace PawnStorages.Factory;
-
-public class CompProperties_FactoryProducer : CompProperties
-{
-    public CompProperties_FactoryProducer() => compClass = typeof(CompFactoryProducer);
-}
